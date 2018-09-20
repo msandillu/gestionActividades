74ac80f23ac7a7da303d86bd0a86db96565e59df
Maximiliano Sandillú <msandillu@kaitzen-solutions.com>
2018-09-20 12:33:39 -0300

    Added classes API User

--- src/main/java/com/prueba/administradortarea/user/api/APIUser.java
+++ src/main/java/com/prueba/administradortarea/user/api/APIUser.java
@@ -1,4 +1,10 @@
 package com.prueba.administradortarea.user.api;
 
-public class APIUser {
+public interface APIUser {
+
+    String login(String username, String password);
+
+    String getUsers();
+
+    String getUsersById(Integer userId);
 }
--- src/main/java/com/prueba/administradortarea/user/api/APIUserImpl.java
+++ src/main/java/com/prueba/administradortarea/user/api/APIUserImpl.java
@@ -1,4 +1,46 @@
 package com.prueba.administradortarea.user.api;
 
-public class APIUserImpl {
+
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.config.ConnectionConfig;
+
+import javax.print.DocFlavor;
+import javax.xml.bind.JAXBContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class APIUserImpl implements APIUser {
+
+    private static final String loginURL = "https://gentle-eyrie-95237.herokuapp.com/login";
+
+    public String login (String username, String password){
+        HttpClient httpClient;
+
+        try {
+            URL targetUrl = new URL(loginURL);
+            HttpURLConnection httpConnection = (HttpURLConnection) targetUrl.openConnection();
+            httpConnection.setDoOutput(true);
+            httpConnection.setRequestMethod("POST");
+            httpConnection.setRequestProperty("Content-Type", "application/json");
+
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public String getUsers(){
+        return "";
+    }
+
+    public String getUsersById(Integer userId){
+        return "";
+    }
 }
